e21603d788d2ec446e0598ae80ed6d665461e195
Deflinger <cesaralejandrovirgenherrera@gmail.com>
2025-02-28 14:12:08 -0600

    Creacion de Fragments y uso de Props

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,27 @@
 
-
-function Card() {
-    
+interface Cardprops {
+    body: string
+}
+function Card(props : Cardprops) {  
+    const {body} = props;
     return (
-    <div 
-    className="card" 
-    style={{
-        width : "350px",
-    }}
+    <div className="card" style={{
+        width : "350px",}}
     >
         <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+            {body}
         </div>
-  </div>);
+    </div>
+  );
+}
+
+export function CardBody(){
+    return(
+    <>
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+    </>)
 }
 
 export default Card;
