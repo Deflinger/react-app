ce58023cb26cb4d1c6784f05f3a799e71f607139
Deflinger <cesaralejandrovirgenherrera@gmail.com>
2025-03-01 19:27:59 -0600

    Renderizado condicional

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import Card ,{CardBody} from "./components/Card";
 import List from "./components/List";
 
 function App() {
-    const list=
-    ["Tanjiro",
-    "Goku",
-    "Eren"];
+    const list= ["Tanjiro", "Goku", "Eren"];
+    const handleSelect = (elemento:string) => {
+        console.log('Imprimiendo', elemento);
+    }
+    const handleSelect2 = (elemento:string) => {
+        console.log('Mostrando', elemento);
+    }
+
+    const contenido = list.length ? (
+    <List data={list} onSelect={handleSelect}/>    
+    ):(
+        'sin elementos para mostrar'
+      )
+    ; 
  return <Card>
+            {list.length !== 0 && "mi lista"}
             <CardBody title="Hola mundo" text="El texto"/>
-            <List data={list}/>
-            <List data={list}/>
+            {contenido}
+            <List data={list} onSelect={handleSelect2}/>
         </Card>;
 }
 
