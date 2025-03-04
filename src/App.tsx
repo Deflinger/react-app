a615cc3eb62ed20cbe141de1ecde2c95e42874ff
Deflinger <cesaralejandrovirgenherrera@gmail.com>
2025-03-04 13:32:45 -0600

    Gestion de Boton y Gesion de estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import Button from "./components/Button";
 import Card ,{CardBody} from "./components/Card";
 import List from "./components/List";
 
 function App() {
+
+    const [isLoading, setisLoading]=useState(false);
+    const handleClick= () => setisLoading(!isLoading);
+
     const list= ["Tanjiro", "Goku", "Eren"];
     const handleSelect = (elemento:string) => {
         console.log('Imprimiendo', elemento);
@@ -21,6 +27,9 @@ function App() {
             <CardBody title="Hola mundo" text="El texto"/>
             {contenido}
             <List data={list} onSelect={handleSelect2}/>
+            <Button isLoading={isLoading} onClick={handleClick} >
+                Hola mundo
+            </Button>
         </Card>;
 }
 
